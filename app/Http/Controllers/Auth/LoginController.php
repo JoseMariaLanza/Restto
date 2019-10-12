ea8f4d9ade34f4581b989bca88bb8fef9fdf6a16
José María Lanza <lanza.josemaria@gmail.com>
2019-10-12 00:16:14 -0300

    Agregado controlador EmpleadosController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/detalles/{id}';
 
     /**
      * Create a new controller instance.
