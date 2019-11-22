b7085d9fa4c29d13d3d8b5bd66591079732980e2
José María Lanza <lanza.josemaria@gmail.com>
2019-11-22 02:10:23 -0300

    Agregado logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,10 +21,11 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <!-- Original: bg-white -->
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color:#363435;">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Restto') }}
+                <a class="navbar-brand" href="{{ url('/ventas/create') }}">
+                    <img src="/images/Caprice-Logo.jpeg" alt="CAPRICE" height="50" width="50">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -34,28 +35,28 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                     @can('menu.index')
-                        <a class="navbar-brand" href="{{ route('menu.index') }}">Menú</a>
+                        <a class="navbar-brand text-white" href="{{ route('menu.index') }}">Menú</a>
                     @endcan
                     @can('mesas.index')
-                        <a class="navbar-brand" href="{{ route('mesas.index') }}">Mesas</a>
+                        <a class="navbar-brand text-white" href="{{ route('mesas.index') }}">Mesas</a>
                     @endcan
                     @can('cajas.index')
-                        <a class="navbar-brand" href="{{ route('cajas.index') }}">Caja</a>
+                        <a class="navbar-brand text-white" href="{{ route('cajas.index') }}">Caja</a>
                     @endcan
                     @can('users.index')
-                        <a class="navbar-brand" href="{{ route('users.index') }}">Usuarios</a>
+                        <a class="navbar-brand text-white" href="{{ route('users.index') }}">Usuarios</a>
                     @endcan
                     @can('roles.index')
-                        <a class="navbar-brand" href="{{ route('roles.index') }}">Roles</a>
+                        <a class="navbar-brand text-white" href="{{ route('roles.index') }}">Roles</a>
                     @endcan
                     @can('gastos.index')
-                        <a class="navbar-brand" href="{{ route('gastos.index') }}">Gastos</a>
+                        <a class="navbar-brand text-white" href="{{ route('gastos.index') }}">Gastos</a>
                     @endcan
                     @can('ventas.index')
-                        <a class="navbar-brand" href="{{ route('ventas.index') }}">Ventas</a>
+                        <a class="navbar-brand text-white" href="{{ route('ventas.index') }}">Ventas</a>
                     @endcan
                     @can('ventas.create')
-                        <a class="navbar-brand" href="{{ route('ventas.create') }}">Vender</a>
+                        <a class="navbar-brand text-white" href="{{ route('ventas.create') }}">Vender</a>
                     @endcan
                     </ul>
 
@@ -73,7 +74,7 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
